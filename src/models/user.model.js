71c8f54f4f2e1555d9b2c8bb9ec64b39a7a3909e
Kuldeep <kunalrajput67718@gmail.com>
2025-08-04 16:51:02 +0530

    add register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -43,16 +43,16 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", function (next) {
+userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 userSchema.methods.generateAccessToken = function () {
- return jwt.sign(
+  return jwt.sign(
     {
       _id: this._id,
       email: this.email,
@@ -66,10 +66,9 @@ userSchema.methods.generateAccessToken = function () {
   );
 };
 userSchema.methods.generateRefreshToken = function () {
-     return jwt.sign(
+  return jwt.sign(
     {
       _id: this._id,
-     
     },
     process.env.REFRESH_TOKEN_SECRET,
     {
